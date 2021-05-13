12bbf07c909ebdef6538b9213119f79ff1513042
Anurag Kotwal <anuragjamliya85@gmail.com>
2021-05-13 12:43:18 +0530

    initial commit

---
+++ loginapp/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'LoginPage.dart';
+import 'Details.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      //home: LoginPage(),
+      initialRoute: '/',
+      //here is the route
+      routes: {
+        '/': (context) => LoginPage(),// navigate to login page
+        '/second': (context) => SecondScreen(),// navigate to Second screen
+      }
+    );
+  }
+}
